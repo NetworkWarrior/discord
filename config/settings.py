7f6645c3596323751edcbf3fd7d30ac40f5722a5
NetworkWarrior <ismoiljonabdumajidov623@gmail.com>
2022-12-30 03:36:13 +0100

    Add static root

--- config/settings.py
+++ config/settings.py
@@ -131,7 +131,7 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
-STATIC_ROOT = str(BASE_DIR.joinpath('static-files'))
+STATIC_ROOT = 'static-files'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
