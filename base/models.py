079679b51cfd9e990473a394b15fc7d78f3f7b39
NetworkWarrior <110435530+NetworkWarrior@users.noreply.github.com>
2025-04-19 17:52:09 +0200

    Update models.py

--- base/models.py
+++ base/models.py
@@ -8,7 +8,7 @@ class User(AbstractUser):
     name = models.CharField(max_length=200, null=True)
     email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True, blank=True)
-    avatar = models.ImageField(null=True, default='avatar.svg')
+    avatar = models.ImageField(upload_to='images/', null=True, default='images/avatar.svg')
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
